5ee91554422cfcc2e67a03d87fdc3ab1808f99f6
Perimny84 <P3r1mny007@gmail.com>
2025-04-29 22:00:07 -0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -9,7 +9,7 @@ const default_config = {
   tabs: [
     {
       name: "home",
-      background_url: `src/img/banners/cbg-4.gif`,
+      background_url: `src/img/banners/cbg-10.gif`,
       categories: [
         {
           name: "",
